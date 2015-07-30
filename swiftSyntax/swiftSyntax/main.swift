9fae58734f6244ebb651cd11e0bbe8cfe30b726e
Terry <ytdxxt10@163.com>
2015-07-30 23:24:06 +0800

    add optional and 内存

--- swiftSyntax/swiftSyntax/main.swift
+++ swiftSyntax/swiftSyntax/main.swift
@@ -267,3 +267,160 @@ struct Stack<T> {
 var intStack = Stack<Int>()
 intStack.push(50)
 let lastIn = intStack.pop()
+
+
+/*
+
+可能值类型 表示可能没有值，两种状态 没有值nil（与OC中nil不同）有值
+
+非可能值类型不能为空
+*/
+var optionalNumber:Int?
+optionalNumber = 6
+
+//使用可能值
+
+func findIndexOfString(string: String,array: [String])->Int? {
+   
+    for (index,value) in enumerate(array) {
+    
+        if value == string {
+             return index
+        }
+    }
+
+    return nil
+}
+
+
+var fruit = ["apple", "banana","pear"]
+let index = findIndexOfString("banana", fruit)
+//拆包前 确保 可能值不为空
+if (index != nil) {
+
+    println("I like \(fruit[index!])")
+
+} else {
+
+    println("NO Fruit");
+
+}
+
+//可能值绑定
+
+if let indexValue = index {
+    println("I like \(fruit[indexValue])")
+
+} else {
+
+    println("No fruit")
+}
+
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var address: Address?
+}
+
+class Address {
+    var buildingNumber: String?
+    var streetName: String?
+    var apartmentNumber: String?
+}
+
+let alex = Person()
+let residence = Residence()
+alex.residence = residence
+
+let address = Address()
+address.buildingNumber = "1121"
+address.streetName = "St Marc"
+residence.address = address
+
+
+var addressNumber: Int?
+
+if let residence = alex.residence {
+    if let address = residence.address {
+        if let buildingNumber = address.buildingNumber {
+            if let convertedNumber = buildingNumber.toInt() {
+            
+                 addressNumber = convertedNumber
+            }
+        }
+    }
+
+}
+
+//上述为了获得buildingNumber 太麻烦，可以使用可能值链  会自动检查是否有值
+
+addressNumber = alex.residence?.address?.buildingNumber?.toInt()
+//addressNumber 仍然为一个可能值
+println(addressNumber)
+
+//可能值练结合可能值绑定使用
+
+if let addressNumber = alex.residence?.address?.buildingNumber?.toInt() {
+
+ println(addressNumber)
+}
+
+enum Optional<T> {
+   case None
+   case Some(T)
+
+}
+
+/*使用可能值技术，可以安全操作一些可能没有值的便利
+如果没有值 即为nil
+如果有值，这个值被包装到可能值中
+
+
+对可能值拆包，才可以返回内部存储的那个不是nil的值
+在你确认有值的时候，在使用强制拆包运算符！
+使用 if let 可能值绑定技术，可达到同时检测 和拆包的目的
+
+
+可能值练（？）时一种简洁德方式去应对一串可能值
+*/
+
+
+/*
+内存管理
+swift 不支持手动内存管理
+ARC 不是 垃圾回收机制
+ARC时不需要该对象时立即回收，垃圾回收机制类似于定期清理
+
+
+弱引用 一定是可能值类型
+对可能值进行绑定会产生一个强引用
+
+可能值链在绑定时所产生的强引用并不会被保存到下一行代码
+weak 弱引用
+unowned 非持有引用
+
+
+对一般对象所属情况值使用强引用 比如 局部变量 对象属性
+
+对不同生命线的对象之间的关系使用弱引用
+
+对同生命线的反向引用使用非持有引用
+
+当引用需要反向指回时，如果还使用强引用会产生引用回路，从而产生内存泄露
+
+此时选择使用弱引用或非持有引用
+
+弱引用可以为空，而飞持有引用不能为空
+*/
+
+//if let person = apt.person {
+//
+//Person.dingdong()
+//
+//}
+//上面代码缩写
+//apt.person?.dingdong()
+
